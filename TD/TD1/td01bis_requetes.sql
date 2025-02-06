c35c81b9ba0afe7773cfd8dfe23044e8852eca4b
Esmeralda <esmeralda.martin206@gmail.com>
2025-02-06 12:14:58 +0100

    Update td01bis_requetes.sql

--- TD/TD1/td01bis_requetes.sql
+++ TD/TD1/td01bis_requetes.sql
@@ -48,18 +48,54 @@ FROM qte
 WHERE quantite_max>=10;
 
 --(h) Le nom et la quantité minimum fournie pour chaque produit originaire de Dijon.
-
+SELECT DISTINCT nomp, MIN(quantite)
+FROM fourniture
+INNER JOIN produit ON fourniture.noproduit = produit.noproduit
+WHERE origine='Dijon'
+GROUP BY nomp;
 
 --(i) Le nombre de fournisseurs différents par couleurs de produits.
-
+SELECT COUNT(fourniture.nofour) AS nbFour, couleur
+FROM fourniture
+INNER JOIN produit ON fourniture.noproduit = produit.noproduit
+GROUP BY couleur;
 
 --(j) Les numéros des fournisseurs ayant fourni la plus grande variété de produits.
-
+SELECT COUNT(DISTINCT noproduit) AS variete, nofour
+FROM fourniture
+GROUP BY nofour;
 
 --(k) Les numéros et noms des fournisseurs qui fournissent la plus grande quantité cumulée de produits verts.
-
+SELECT fourniture.nofour, nomf, SUM(fourniture.quantite) AS qteVert
+FROM fourniture
+INNER JOIN fournisseur ON fourniture.nofour = fournisseur.nofour
+INNER JOIN produit ON fourniture.noproduit = produit.noproduit
+WHERE couleur='Vert'
+GROUP BY fourniture.nofour, nomf;
 
 --(l) Les numéros des fournisseurs qui fournissent tous les produits originaires de « Dijon ». On recherchera les fournisseurs dont l’image dans FOURNITURE est égale à l’ensemble des produits originaires de dijon.
+WITH proDij AS(
+SELECT noproduit
+FROM produit
+WHERE origine='Dijon')
+SELECT fourniture.nofour,fourniture.noproduit
+FROM fourniture
+INNER JOIN fournisseur ON fourniture.nofour = fournisseur.nofour
+INNER JOIN proDij ON fourniture.noproduit = proDij.noproduit
+GROUP BY fourniture.nofour, fourniture.noproduit;
+
+--Correction du (l)
+SELECT f1.nofour
+FROM fournisseur f1
+WHERE NOT EXISTS (
+SELECT p.noproduit
+FROM produit p
+WHERE p.origine='Dijon'
+AND NOT EXISTS (
+SELECT *
+FROM fourniture f2
+WHERE f2.noproduit = p.noproduit
+AND f2.nofour = f1.nofour));
 
 --(m) Les numéros des fournisseurs qui fournissent tous les produits originaires de leur ville.
 
@@ -68,4 +104,10 @@ WHERE quantite_max>=10;
 
 
 --(o) Les couples de numéros de fournisseurs qui ne fournissent aucun produit en commun.
-
+SELECT DISTINCT f1.nofour, f2.nofour FROM fournisseur f1, fournisseur f2
+WHERE f1.nofour < f2.nofour
+AND NOT EXISTS (
+SELECT 1 FROM fourniture do1
+JOIN fourniture fo2 ON fo1.noproduit=fo2.noproduit
+WHERE fo1.nofour=f1.nofour
+AND fo2.nofour=f2.nofour);
